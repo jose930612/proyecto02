e183a166085f499d8ffdbd667e3e4e3273b3739d
Jose Mejia <jamo126@gmail.com>
2019-09-19 16:27:52 -0500

    endpoint

--- setup_general.sh
+++ setup_general.sh
@@ -3,3 +3,5 @@ sudo amazon-linux-extras install docker
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/download/1.25.0-rc2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
